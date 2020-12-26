3fc39b4e57df313d66b25b34fc3310ae7926a5d8
SHUBHI NIGAM <54998601+ShubhiNigam29@users.noreply.github.com>
2020-12-26 07:00:11 -0500

    First Slider

---
+++ README.md
@@ -0,0 +1,3 @@
+# Reusable Concentric Circular Slider
+
+This slider is built with vanilla JS. It's reusable, and customizable.
---
+++ app.js
@@ -0,0 +1,249 @@
+class Slider {
+
+    /**
+     * @constructor
+     * 
+     * @param {string} DOM selector
+     * @param {array} sliders
+     */
+    constructor({ DOMselector, sliders }) {
+        this.DOMselector = DOMselector;
+        this.container = document.querySelector(this.DOMselector);  // Slider container
+        this.sliderWidth = 450;                                     // Slider width
+        this.sliderHeight = 450;                                    // Slider length
+        this.cx = this.sliderWidth / 2;                             // Slider center X coordinate
+        this.cy = this.sliderHeight / 2;                            // Slider center Y coordinate
+        this.tau = 2 * Math.PI;                                     // Tau constant
+        this.sliders = sliders;                                     // Sliders array with opts for each slider
+        this.arcFractionSpacing = 0.85;                             // Spacing between arc fractions
+        this.arcFractionLength = 10;                                // Arc fraction length
+        this.arcFractionThickness = 25;                             // Arc fraction thickness
+        this.arcBgFractionColor = '#D8D8D8';                        // Arc fraction color for background slider
+        this.handleFillColor = '#fff';                              // Slider handle fill color
+        this.handleStrokeColor = '#888888';                         // Slider handle stroke color
+        this.handleStrokeThickness = 3;                             // Slider handle stroke thickness    
+    }
+
+    /**
+     * Draw sliders on init
+     * 
+     */
+    draw() {
+
+        // Create legend UI
+        this.createLegendUI();
+
+        // Create and append SVG holder
+        const svgContainer = document.createElement('div');
+        svgContainer.classList.add('slider__data');
+        const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+        svg.setAttribute('height', this.sliderWidth);
+        svg.setAttribute('width', this.sliderHeight);
+        svgContainer.appendChild(svg);
+        this.container.appendChild(svgContainer);
+
+        // Draw sliders
+        this.sliders.forEach((slider, index) => this.drawSingleSliderOnInit(svg, slider, index));
+    }
+
+    /**
+     * Draw single slider on init
+     * 
+     * @param {object} svg 
+     * @param {object} slider 
+     * @param {number} index 
+     */
+    drawSingleSliderOnInit(svg, slider, index) {
+
+        // Default slider opts, if none are set
+        slider.radius = slider.radius ?? 50;
+        slider.min = slider.min ?? 0;
+        slider.max = slider.max ?? 1000;
+        slider.step = slider.step ?? 50;
+        slider.initialValue = slider.initialValue ?? 0;
+        slider.color = slider.color ?? '#FF5733';
+
+        // Calculate slider circumference
+        const circumference = slider.radius * this.tau;
+
+        // Calculate initial angle
+        const initialAngle = Math.floor( (slider.initialValue / (slider.max - slider.min)) * 360 );
+
+        // Calculate spacing between arc fractions
+        const arcFractionSpacing = this.calculateSpacingBetweenArcFractions(circumference, this.arcFractionLength, this.arcFractionSpacing);
+
+        // Create a single slider group - holds all paths and handle
+        const sliderGroup = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+        sliderGroup.setAttribute('class', 'sliderSingle');
+        sliderGroup.setAttribute('data-slider', index);
+        sliderGroup.setAttribute('transform', 'rotate(-90,' + this.cx + ',' + this.cy + ')');
+        sliderGroup.setAttribute('rad', slider.radius);
+        svg.appendChild(sliderGroup);
+        
+        // Draw background arc path
+        this.drawArcPath(this.arcBgFractionColor, slider.radius, 360, arcFractionSpacing, 'bg', sliderGroup);
+
+        // Draw active arc path
+        this.drawArcPath(slider.color, slider.radius, initialAngle, arcFractionSpacing, 'active', sliderGroup);
+
+        // Draw handle
+        this.drawHandle(slider, initialAngle, sliderGroup);
+    }
+
+    /**
+     * Output arch path
+     * 
+     * @param {number} cx 
+     * @param {number} cy 
+     * @param {string} color 
+     * @param {number} angle 
+     * @param {number} singleSpacing 
+     * @param {string} type 
+     */
+    drawArcPath( color, radius, angle, singleSpacing, type, group ) {
+
+        // Slider path class
+        const pathClass = (type === 'active') ? 'sliderSinglePathActive' : 'sliderSinglePath';
+
+        // Create svg path
+        const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+        path.classList.add(pathClass);
+        path.setAttribute('d', this.describeArc(this.cx, this.cy, radius, 0, angle));
+        path.style.stroke = color;
+        path.style.strokeWidth = this.arcFractionThickness;
+        path.style.fill = 'none';
+        path.setAttribute('stroke-dasharray', this.arcFractionLength + ' ' + singleSpacing);
+        group.appendChild(path);
+    }
+
+    /**
+     * Draw handle for single slider
+     * 
+     * @param {object} slider 
+     * @param {number} initialAngle 
+     * @param {group} group 
+     */
+    drawHandle(slider, initialAngle, group) {
+
+        // Calculate handle center
+        const handleCenter = this.calculateHandleCenter(initialAngle * this.tau / 360, slider.radius);
+
+        // Draw handle
+        const handle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+        handle.setAttribute('class', 'sliderHandle');
+        handle.setAttribute('cx', handleCenter.x);
+        handle.setAttribute('cy', handleCenter.y);
+        handle.setAttribute('r', this.arcFractionThickness / 2);
+        handle.style.stroke = this.handleStrokeColor;
+        handle.style.strokeWidth = this.handleStrokeThickness;
+        handle.style.fill = this.handleFillColor;
+        group.appendChild(handle);
+    }
+
+    /**
+     * Create legend UI on init
+     * 
+     */
+    createLegendUI() {
+
+        // Create legend
+        const display = document.createElement('ul');
+        display.classList.add('slider__legend');
+
+        // Legend heading
+        const heading = document.createElement('h4');
+        heading.innerText = 'ADJUST DIAL TO ENTER EXPENSES';
+        display.appendChild(heading);
+
+        // Legend data for all sliders
+        this.sliders.forEach((slider, index) => {
+            const li = document.createElement('li');
+            li.setAttribute('data-slider', index);
+            const firstSpan = document.createElement('span');
+            firstSpan.style.backgroundColor = slider.color ?? '#FF5733';
+            firstSpan.classList.add('colorSquare');
+            const secondSpan = document.createElement('span');
+            secondSpan.innerText = slider.displayName ?? 'Unnamed value';
+            const thirdSpan = document.createElement('span');
+            thirdSpan.innerText = slider.initialValue ?? 0;
+            thirdSpan.classList.add('sliderValue');
+            li.appendChild(firstSpan);
+            li.appendChild(secondSpan);
+            li.appendChild(thirdSpan);
+            display.appendChild(li);
+        });
+
+        // Append to DOM
+        this.container.appendChild(display);
+    }
+
+    /**
+     * Calculate number of arc fractions and space between them
+     * 
+     * @param {number} circumference 
+     * @param {number} arcBgFractionLength 
+     * @param {number} arcBgFractionBetweenSpacing 
+     * 
+     * @returns {number} arcFractionSpacing
+     */
+    calculateSpacingBetweenArcFractions(circumference, arcBgFractionLength, arcBgFractionBetweenSpacing) {
+        const numFractions = Math.floor((circumference / arcBgFractionLength) * arcBgFractionBetweenSpacing);
+        const totalSpacing = circumference - numFractions * arcBgFractionLength;
+        return totalSpacing / numFractions;
+    }
+
+    /**
+     * Helper functiom - describe arc
+     * 
+     * @param {number} x 
+     * @param {number} y 
+     * @param {number} radius 
+     * @param {number} startAngle 
+     * @param {number} endAngle 
+     * 
+     * @returns {string} path
+     */
+    describeArc (x, y, radius, startAngle, endAngle) {
+        let endAngleOriginal, start, end, arcSweep, path;
+        endAngleOriginal = endAngle;
+
+        if(endAngleOriginal - startAngle === 360){
+            endAngle = 359;
+        }
+
+        start = this.polarToCartesian(x, y, radius, endAngle);
+        end = this.polarToCartesian(x, y, radius, startAngle);
+        arcSweep = endAngle - startAngle <= 180 ? '0' : '1';
+
+        if (endAngleOriginal - startAngle === 360) {
+            path = [
+                'M', start.x, start.y,
+                'A', radius, radius, 0, arcSweep, 0, end.x, end.y, 'z'
+            ].join(' ');
+        } else {
+            path = [
+                'M', start.x, start.y,
+                'A', radius, radius, 0, arcSweep, 0, end.x, end.y
+            ].join(' ');
+        }
+
+        return path;
+    }
+
+    /**
+     * Helper function - polar to cartesian transformation
+     * 
+     * @param {number} centerX 
+     * @param {number} centerY 
+     * @param {number} radius 
+     * @param {number} angleInDegrees 
+     * 
+     * @returns {object} coords
+     */
+     polarToCartesian (centerX, centerY, radius, angleInDegrees) {
+        const angleInRadians = angleInDegrees * Math.PI / 180;
+        const x = centerX + (radius * Math.cos(angleInRadians));
+        const y = centerY + (radius * Math.sin(angleInRadians));
+        return {x, y};
+    }
+}
